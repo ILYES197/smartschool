fe3b96373bb424f27630237c9745558476d2865d
ilyes <baghailyes97@gmail.com>
2024-07-02 10:29:45 +0200

    errors

--- smartschool/formation/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import formation
-
-class FormationSerializer(serializers.ModelSerializer):
-     class Meta:
-        model = formation
-        fields = "__all__"
