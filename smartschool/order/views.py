6a53cbc7e8d41df9e7a74d5f5194fbd2a0583c0a
ilyes <baghailyes97@gmail.com>
2024-07-10 13:40:18 +0200

    add api formations of user

--- smartschool/order/views.py
+++ smartschool/order/views.py
@@ -2,9 +2,15 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated ,IsAdminUser
+from rest_framework import status
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, OrderItemsSerializer
 from formation.models import formation
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from .models import Order
+ 
 
 class CreateOrderView(APIView):
 
@@ -60,10 +66,10 @@ class CreateOrderView(APIView):
         else:
             return Response(order_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Add to your urls.py
-from django.urls import path
-from .views import CreateOrderView
+class UserOrdersView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-urlpatterns = [
-    path('create-order/', CreateOrderView.as_view(), name='create-order'),
-]
+    def get_queryset(self):
+        user = self.request.user
+        return Order.objects.filter(user=user)
